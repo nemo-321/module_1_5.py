48491dabe0ce7496ffe1f9712c514bce56a1c10c
nemo-321 <alexagolubev24@gmail.com>
2024-11-27 12:24:41 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,7 @@
+immutable_var=tuple([1,2,3,4,"car","aple"])
+print(immutable_var)
+print(type(immutable_var))
+immutable_list=["aple","banana","6","8"]
+print(immutable_list)
+print(type(immutable_list))
+print(immutable_list[::-1])
